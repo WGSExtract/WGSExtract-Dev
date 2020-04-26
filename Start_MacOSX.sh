53924c008f39ddd6f52d96f2491b1024b5f1c3e4
Randy H <58348151+RandyHarr@users.noreply.github.com>
2020-04-25 22:45:43 -0400

    Repaired path for script running

--- Start_MacOSX.sh
+++ Start_MacOSX.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo "Starting WGSExtract..."
-/usr/local/bin/python3 ./programs/wgsextract/wgsextract.py
+WGSEDIR=`dirname "$0"`
+WGSEABS=`cd "$WGSEDIR"; pwd`
+/usr/local/bin/python3 $WGSEABS/programs/wgsextract/wgsextract.py
